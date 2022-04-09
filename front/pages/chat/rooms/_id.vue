3ebeee1592ba703810c6fd68bb78eef017222671
leouno <leouno12@gmail.com>
2022-04-09 12:44:28 +0900

    feat: enable some markdown style and show URL as link

--- front/pages/chat/rooms/_id.vue
+++ front/pages/chat/rooms/_id.vue
@@ -240,7 +240,14 @@ export default class ChatRoom extends Mixins<FetchUser>(FetchUser) {
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
       .replace(/"/g, '&quot;')
+      .replace(/\n{3,}/g, '\n\n') // trim successive \n in 2 times
       .replace(/\n/g, '<br />')
+      .replace(
+        /(https?:\/\/[-_.!~*'()a-zA-Z0-9/?:@&=+$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+)/g,
+        '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>',
+      ) // convert URL to a tag link
+      .replace(/\*\*([^*]+)\*\*/g, '<strong>$1</strong>') // markdown strong
+      .replace(/\*([^*]+)\*/g, '<em>$1</em>') // markdown italic
   }
 
   resetInfiniteLoading() {
