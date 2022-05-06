cbf3e1518e41b506ec9d78340ce2d1b268d9a383
leouno <leouno12@gmail.com>
2022-05-06 11:39:20 +0900

    feat: enable scrolling only in chatroom list area

--- front/components/ChatSideBar.vue
+++ front/components/ChatSideBar.vue
@@ -6,30 +6,34 @@
       app
       fixed
     >
-      <v-list
-        class="scrollable-list"
-      >
-        <v-list-item>
+      <v-row>
+        <v-col>
           <v-spacer></v-spacer>
           <v-btn @click="click">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
-        </v-list-item>
-        <v-list-item
-          v-for="(item, i) in roomItems"
-          :key="i"
-          :to="item.to"
-          exact
-          router
-        >
-          <v-list-item-action>
-            <v-icon>mdi-chat-processing-outline</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.room.label"/>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+        </v-col>
+      </v-row>
+      <v-container class="fill-height">
+        <div class="inherit-hw">
+          <v-list class="scrollable-list">
+            <v-list-item
+              v-for="(item, i) in roomItems"
+              :key="i"
+              :to="item.to"
+              exact
+              router
+            >
+              <v-list-item-action>
+                <v-icon>mdi-chat-processing-outline</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.room.label"/>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </div>
+      </v-container>
     </v-navigation-drawer>
   </div>
 </template>
@@ -40,19 +44,11 @@ import { RoomItem } from '~/layouts/chat.vue'
 
 @Component
 export default class ChatSideBar extends Vue {
-  @Prop({default: null})
-  private value!: boolean | null
-
   @Prop({required: true, default: []})
   roomItems!: RoomItem[]
 
-  @Emit('click-create-room')
-  click() {
-    // do nothing
-  }
-
-  @Emit()
-  public input(_value: boolean | null) {}
+  @Prop({default: null})
+  private value!: boolean | null
 
   private get drawer(): boolean | null {
     return this.value
@@ -62,12 +58,35 @@ export default class ChatSideBar extends Vue {
     this.input(value)
   }
 
+  @Emit('click-create-room')
+  click() {
+    // do nothing
+  }
+
+  @Emit()
+  public input(_value: boolean | null) {
+  }
+
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+/* Disable scrolling navigation drawer */
+::v-deep .v-navigation-drawer__content { /* stylelint-disable-line selector-class-pattern */
+  overflow: hidden
+}
+
+.inherit-hw {
+  height: inherit;
+  width: inherit;
+}
+
+/* Enable scrolling only in chatroom list area */
 .scrollable-list {
-  -webkit-overflow-scrolling: auto;
-  overflow-y: auto;
+  padding: 0;
+  height: calc(100% - 70px);
+
+  /* -webkit-overflow-scrolling: auto; */
+  overflow-y: scroll;
 }
 </style>
