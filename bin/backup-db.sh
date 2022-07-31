c7ec5103cb9c4482f883bea246d48225922cffc7
leouno <leouno12@gmail.com>
2022-07-31 10:54:31 +0900

    modify backup script

--- bin/backup-db.sh
+++ bin/backup-db.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -eux
 
+
 SERVICE_NAME=db
+HOST_BAK_DIR=backup
+
+mkdir -p $HOST_BAK_DIR
 
 # These values should be read from .env.prod
 SQL_USER=db_user_name
@@ -14,5 +18,5 @@ BAK_FILE=bak_${SQL_DATABASE}_`date +'%Y%m%d-%H%M%S'`.sql
 bin/prod exec -e BAK_FILE=$BAK_FILE $SERVICE_NAME bash -c "pg_dump -U $SQL_USER $SQL_DATABASE > /tmp/$BAK_FILE"
 
 # Copy to host
-docker cp `bin/prod ps -q $SERVICE_NAME`:/tmp/${BAK_FILE} ./backup/$BAK_FILE
+docker cp `bin/prod ps -q $SERVICE_NAME`:/tmp/${BAK_FILE} ./${HOST_BAK_DIR}/${BAK_FILE}
 
